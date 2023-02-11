ef065cc2747c5994501a0c3c4b33f1311daa79da
Mac <mhein94@gmail.com>
2023-02-11 13:50:11 -0800

    loaded a couple of libraries and added some comments about what I need to do.

--- Submission 1 (Course Week 4)/Scripts/TidyTuesday1BigTech.R
+++ Submission 1 (Course Week 4)/Scripts/TidyTuesday1BigTech.R
@@ -4,5 +4,14 @@
 
 # Load Libraries ----
 
+library(tidverse)
+library(here)
+# Need to figure out which other libraries to use
+
 # Load Data ----
+
+# Need to ask if I should use the big tech stock data or another data set
+# Need to figure out how to add a csv file to the Data folder in the project
+
+# Data Analysis ----
 
