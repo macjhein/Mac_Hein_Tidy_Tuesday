56250886c658ea2d5a88378fe54aac890497f83a
Mac <mhein94@gmail.com>
2023-02-14 10:25:49 -0800

    Added data set."

--- Submission 1 (Course Week 4)/Scripts/TidyTuesday1BigTech.R
+++ Submission 1 (Course Week 4)/Scripts/TidyTuesday1BigTech.R
@@ -13,5 +13,7 @@ library(here)
 # Need to ask if I should use the big tech stock data or another data set
 # Need to figure out how to add a csv file to the Data folder in the project
 
+big_tech_stock_prices <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-02-07/big_tech_stock_prices.csv')
+
 # Data Analysis ----
 
