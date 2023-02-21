90748bce61d339e3905a835446bc8d20e1112fce
Mac <mhein94@gmail.com>
2023-02-21 14:46:07 -0800

    Adding files for second submission.

---
+++ Submission 2 (Course Week 5)/Scripts/TidyTuesday2BobRoss.R
@@ -0,0 +1,23 @@
+# Tidy Tuesday Submission 1 - Big Tech Stock ----
+# Created By: Mac Hein
+# Created On: 2023-02-21
+
+# Load Libraries ----
+
+library(tidyverse)
+library(here)
+library(devtools)
+library(ggdist)
+library(tidytuesdayR)
+
+# Load Data ----
+
+tuesdata <- tidytuesdayR::tt_load('2023-02-21')
+tuesdata <- tidytuesdayR::tt_load(2023, week = 8)
+
+bob_ross <- tuesdata$bob_ross
+
+# Data Analysis ----
+
+View(bob_ross)
+
